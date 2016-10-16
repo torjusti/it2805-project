26a1fb7dbe086967e21628239c6f36dbea808524
Balázs Orbán <info@balazsorban.com>
2016-10-16 02:07:32 +0200

    Tried some animations.

--- docs/assets/js/global.js
+++ docs/assets/js/global.js
@@ -1,14 +1,15 @@
-// Når skjermen er mindre enn en viss bredde gjemmes navigasjonsbaren og byttes ut med en knapp.
-// Når knappen klikkes, vil vi veksle mellom at navigasjonsbaren skal vises eller ikke.
+/*Når skjermen er mindre enn en viss bredde gjemmes navigasjonsbaren og byttes ut med en knapp.
+ Når knappen klikkes, vil vi veksle mellom at navigasjonsbaren skal vises eller ikke.*/
 var navigationBar = document.getElementById('navigation-bar');
 var menuMobile = document.getElementById('menu-mobile');
 
 menuMobile.addEventListener('click', function() {
-  // Når knappen klikkes for første gang er style.display en tom streng, til tross for at
-  // den finnes i CSS. En enkel løsning er dermed å bare sjekke om vi har en tom streng.
+  /* Når knappen klikkes for første gang er style.display en tom streng, til tross for at
+   den finnes i CSS. En enkel løsning er dermed å bare sjekke om vi har en tom streng.*/
   if (navigationBar.style.display === 'none' || !navigationBar.style.display) {
     navigationBar.style.display = 'flex';
   } else {
+
     navigationBar.style.display = 'none';
   }
 
