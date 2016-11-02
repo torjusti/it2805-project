07cca425596a1aacff6da3dfea2db50b651e76d5
Balázs Orbán <info@balazsorban.com>
2016-11-02 11:46:06 +0100

    Added parallax effect to trivia and moved montering.html content to montering.pug

---
+++ docs/assets/js/trivia.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('scroll', function(){
+    
+    const fromTop = document.getElementsByTagName("BODY")[0].scrollTop;
+    const scrollFifth = document.getElementById("parallax-5");
+
+    document.getElementById("parallax-2").style.top = 100 - fromTop/10 + "%";
+    document.getElementById("parallax-3").style.top = 150 - fromTop/8 + "%";
+    document.getElementById("parallax-4").style.top = 250 - fromTop/7 + "%";
+
+    fromTop > 720 ? scrollFifth.style.display = 'block' : scrollFifth.style.display = 'none';
+
+  });
+});
