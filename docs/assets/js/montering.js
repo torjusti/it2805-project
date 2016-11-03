2853779a2e7e6263014bc3e09e3af2ca9e547dde
Balázs Orbán <info@balazsorban.com>
2016-11-03 02:53:43 +0100

    Moved montering html to pug file, simplified montering.js, and added a primitive styling.

--- docs/assets/js/montering.js
+++ docs/assets/js/montering.js
@@ -1,25 +1,20 @@
+/*Variabler*/
 const buttonValue=["Neste","Neste","Neste","Neste","OBS!","Start på nytt"];
-const stegName=["Steg 1:","Steg 2:","Steg 3:","Steg 4:","Steg 5:","OBS!"]
-/*Henting av html elementer*/
-let monteringButton=document.getElementById("monteringBtn");
-let step=document.getElementById("stepCount");
-let mont=document.getElementsByClassName("monteringCont");
-/*Andre variabler*/
-let monteringIndex=-1;
+let montBtn=document.getElementById("montering-btn");
+let mont=document.getElementsByClassName("montering");
+let montIndex=-1;
 /*Funksjonen for montering tutorialen*/
 function tutorial(){
-  step.style.display="flex";
   for(i=0;i<mont.length;i++){
     mont[i].style.display="none";
   }
-  monteringIndex++;
-  if (monteringIndex>=mont.length){
-    monteringIndex=0;
-    mont[monteringIndex].style.display="flex";
+  montIndex++;
+  if (montIndex>=mont.length){
+    montIndex=0;
+    mont[montIndex].style.display="flex";
   }
-  mont[monteringIndex].style.display="flex";
-  monteringButton.innerHTML=buttonValue[monteringIndex];
-  step.innerHTML=stegName[monteringIndex];
+  mont[montIndex].style.display="flex";
+  montBtn.innerHTML=buttonValue[montIndex];
 }
-/*Eventlistener som fanger opp klikk på knappen*/
-monteringBtn.addEventListener("click",tutorial);
+/*EventListener som fanger opp klikk på knappen*/
+montBtn.addEventListener("click",tutorial);
