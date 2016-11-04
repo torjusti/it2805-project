aa56a16643f3d55d0019063c12d1bb5356a8a775
andreasski <andrefjeld@gmail.com>
2016-11-04 16:08:09 +0100

    Montering navigasjon oppdatert
    La til tilbake funskjonalitet, samt en progresjonsbar.

--- docs/assets/js/montering.js
+++ docs/assets/js/montering.js
@@ -1,20 +1,42 @@
 /*Variabler*/
-const buttonValue=["Neste","Neste","Neste","Neste","OBS!","Start på nytt"];
-let montBtn=document.getElementById("assembly-btn-next");
-let mont=document.getElementsByClassName("assembly");
 let montIndex=-1;
-/*Funksjonen for montering tutorialen*/
-function tutorial(){
-  for(i=0;i<mont.length;i++){
-    mont[i].style.display="none";
+/*HTML elementer hentes*/
+let assemblyIntro=document.getElementById("assembly-intro");
+let progBar=document.getElementById("prog-bar");
+let progBarElement=document.getElementsByClassName("prog-bar-element");
+let assemblyBtnNext=document.getElementById("assembly-btn-next");
+let assemblyBtnBack=document.getElementById("assembly-btn-back");
+let assemblyContent=document.getElementsByClassName("assembly-content");
+/*EventListener som fanger opp klikk på "neste" knappen*/
+assemblyBtnNext.addEventListener("click",function(e){
+  assemblyBtnNext.innerHTML="Neste";
+  progBar.style.display="flex";
+  assemblyBtnBack.style.display="flex";
+  assemblyIntro.style.display="none";
+  for(i=0;i<assemblyContent.length;i++){
+    assemblyContent[i].style.display="none";
+    progBarElement[i].style.color="#999";
   }
   montIndex++;
-  if (montIndex>=mont.length){
+  if (montIndex>=assemblyContent.length){
     montIndex=0;
-    mont[montIndex].style.display="flex";
   }
-  mont[montIndex].style.display="flex";
-  montBtn.innerHTML=buttonValue[montIndex];
-}
-/*EventListener som fanger opp klikk på knappen*/
-montBtn.addEventListener("click",tutorial);
+  else if(montIndex>=assemblyContent.length-1){
+    assemblyBtnNext.innerHTML="Start på nytt";
+  }
+  assemblyContent[montIndex].style.display="flex";
+  progBarElement[montIndex].style.color="#00A651";
+})
+/*EventListener som fanger opp klikk på "tilbake" knappen*/
+assemblyBtnBack.addEventListener("click",function(e){
+  for(i=0;i<assemblyContent.length;i++){
+    assemblyContent[i].style.display="none";
+    progBarElement[i].style.color="#999";
+  }
+  montIndex--;
+  if (montIndex<=-1){
+    montIndex=assemblyContent.length-1;
+  }
+  assemblyContent[montIndex].style.display="flex";
+  progBarElement[montIndex].style.color="#00A651";
+});
