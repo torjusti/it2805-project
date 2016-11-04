bca218bf5ba939c6c4f054c336f8aba7b978d050
Torjus Iveland <torjusti@gmail.com>
2016-11-04 23:16:57 +0100

    Add gulp config for building SCSS.

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -2,9 +2,21 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
+const sass = require('gulp-sass');
 
-gulp.task('default', () =>
+gulp.task('minify', () =>
   gulp.src('assets/img-orig/**')
     .pipe(imagemin([imageminPngquant(), imageminMozjpeg(), imagemin.svgo(), imagemin.gifsicle()])) // default, but with mozjpeg and pngquant
     .pipe(gulp.dest('assets/img'))
 );
+
+gulp.task('sass', () =>
+  gulp.src('assets/css/sass/base.scss')
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(gulp.dest('assets/css/'))
+);
+
+gulp.task('watch', function () {
+  gulp.watch('assets/css/**/*.scss', ['sass']);
+  gulp.watch('assets/img/**', ['minify']);
+});
