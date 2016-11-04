fce147517bffc6aa93dd91fc2c26cfc419faf188
Balázs Orbán <info@balazsorban.com>
2016-11-04 18:00:13 +0100

    Simplifying CSS and HTML;Responsive font sizes.

--- docs/assets/js/global.js
+++ docs/assets/js/global.js
@@ -101,12 +101,15 @@ function createElem(obj) {
   // Når knappen klikkes, vil vi veksle mellom at navigasjonsbaren skal vises eller ikke.
   var navigationBar = document.getElementById('navigation-bar');
   var menuMobile = document.getElementById('menu-mobile');
+  var footer = document.getElementsByTagName('footer');
 
   menuMobile.addEventListener('click', function() {
     // Når knappen klikkes for første gang er style.display en tom streng, til tross for at
     // den finnes i CSS. En enkel løsning er dermed å bare sjekke om vi har en tom streng.
     if (navigationBar.style.display === 'none' || !navigationBar.style.display) {
       navigationBar.style.display = 'flex';
+      footer[0].style.position = 'fixed';
+      footer[0].style.bottom = '0';
       navigationBar.style.animation = 'slideInTop .3s forwards';
     } else {
       navigationBar.style.display = 'none';
@@ -122,6 +125,7 @@ function createElem(obj) {
   for (let i = 0; i < headerLinks.length; i++) {
     if (headerLinks[i].pathname === location.pathname) {
       headerLinks[i].parentNode.classList.add('active-menu');
+      footer[0].style.position = 'static';
       break;
     }
   }
