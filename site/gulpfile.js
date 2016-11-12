6aa9589f94d9f453ab2a4cfa054eb24550cc8808
Torjus Iveland <torjusti@gmail.com>
2016-11-12 14:06:45 +0100

    busywork

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -2,8 +2,6 @@ const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
-const sass = require('gulp-sass');
-const pug = require('gulp-pug');
 const livereload = require('gulp-livereload');
 
 gulp.task('minify', () =>
@@ -12,24 +10,8 @@ gulp.task('minify', () =>
     .pipe(gulp.dest('assets/img'))
 );
 
-gulp.task('sass', () =>
-  gulp.src('assets/css/sass/base.scss')
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(gulp.dest('assets/css/'))
-    .pipe(livereload())
-);
-
-gulp.task('pug', () =>
-  gulp.src('pug/*.pug')
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest('.'))
-    .pipe(livereload())
-);
-
 gulp.task('watch', function (){
   livereload.listen();
   livereload.reload();
-  gulp.watch('assets/css/**/*.scss', ['sass']);
   gulp.watch('assets/img/**', ['minify']);
-  gulp.watch('pug/**/*.pug', ['pug']);
 });
